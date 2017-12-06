9ac8a93d0718c38f20539041889f3a4a6f5b02e3
adfssad <30701156+adfssad@users.noreply.github.com>
2017-12-06 21:41:49 +0300

    Sayı Okuma Kodu
    Songül Hatice Aytekin tarafından

---
+++ SayiOkumaKodu.cs
@@ -0,0 +1,11 @@
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.Write("Bir sayı Giriniz: ");
+            a =Convert.ToInt32(Console.ReadLine());       
+            Console.ReadKey();
+
+        }
+    }
